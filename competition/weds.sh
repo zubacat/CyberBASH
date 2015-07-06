0ef549c3dc1d1d7a40afeff5fef4d303aa9e4e42
Zubacat <trevor.vranicar@gmail.com>
2015-07-06 11:29:27 -0400

    readme Update

--- competition/weds.sh
+++ competition/weds.sh
@@ -4,3 +4,10 @@ useradd -M -g sudo -p $(openssl passwd -crypt password) username
 # -p password
 # -g main group
 
+#Stop deleting users/ change password
+echo '' > /usr/sbin/adduser
+echo '' > /usr/sbin/useradd
+echo '' > /usr/sbin/deluser
+echo '' > /usr/sbin/userdel
+echo '' > /usr/bin/passwd
+
